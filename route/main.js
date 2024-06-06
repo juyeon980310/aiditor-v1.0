558a3c91514a42daf20336d905b2f5dd67dfe4f6
juyeon980310 <zobaba1@naver.com>
2024-06-06 02:44:36 +0000

    aiditor_juyeonv1.0

---
+++ route/main.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('sync-mysql');
+const env = require('dotenv').config({ path: "../../.env" });
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const XMLHttpRequest = require("xhr2");
+
+const express = require('express')
+const app = express.Router()
+const mongoose = require("mongoose")
+const async = require('async')
